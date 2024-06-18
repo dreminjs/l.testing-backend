796ef6a9d39be6d24e4aa452187e8fc87c8b076f
Andrey Dremin <sankoa230@gmail.com>
2024-06-18 15:02:51 +0300

    one more changes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.enableCors({ credentials: true, origin: true })
+	console.log(__dirname)
 	await app.listen(8077)
 }
 bootstrap()
