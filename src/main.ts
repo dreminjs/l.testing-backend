a38eca9539397f83054d5b6b3d3c20101e39b050
Andrey Dremin <sankoa230@gmail.com>
2024-06-18 18:38:42 +0300

    changed origin url

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	app.enableCors({ credentials: true, origin: true })
+	app.enableCors({ credentials: true, origin: "https://l-testing-front.vercel.app" })
 	console.log(__dirname)
 	await app.listen(8077)
 }
